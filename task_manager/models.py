6f5442829965e6b45be7e91f97802f66a829fe2d
Olexandr Hontarenko <sanya221138@gmail.com>
2023-12-02 11:44:08 +0200

    create four models with name: Taks, Worker, TaskType and Position.

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,53 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="workers"
+    )
+
+    def __str__(self):
+        return f"User: {self.username}, Position: {self.position}"
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+CHOICES_PRIORITY = (
+    ("URGENT", "Urgent"),
+    ("HIGH", "High"),
+    ("MEDIUM", "Medium"),
+    ("LOW", "Low")
+)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField()
+    priority = models.CharField(max_length=6, choices=CHOICES_PRIORITY)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE, related_name="tasks")
+    assignees = models.ManyToManyField(Worker, related_name="tasks")
+
+    def __str__(self):
+        return (
+            f"Task name: {self.name}, "
+            f"is complicated: {self.is_completed}, "
+            f"Type task: {self.task_type}"
+        )
