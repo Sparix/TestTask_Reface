100366e812ad896890dc35203ec2624e2d48a166
Olexandr Hontarenko <sanya221138@gmail.com>
2023-12-02 18:35:41 +0200

    add path for logout and register user in urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from task_manager.views import index
+from task_manager.views import (
+    index,
+    logout_user,
+    RegisterNewWorkerView
+)
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("account/register/", RegisterNewWorkerView.as_view(), name="register"),
+    path("logout/", logout_user, name="logout"),
 ]
 app_name = "task_manager"
