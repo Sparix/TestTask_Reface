af5b9ac19eff0eae83252324c7cf7a1d08fd0486
Olexandr Hontarenko <sanya221138@gmail.com>
2023-12-02 18:34:28 +0200

    view for logout user, views for RegisterNewUser, and initial page with view name index

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,25 @@
+from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views import generic
+
+from task_manager.forms import RegisterWorkerForm
+from task_manager.models import Worker
 
 
 def index(request):
-    return render(request, "base.html")
+    return render(request, "task_manager/index.html")
+
+
+@login_required
+def logout_user(request):
+    logout(request)
+    return redirect("login")
+
+
+class RegisterNewWorkerView(generic.CreateView):
+    model = Worker
+    form_class = RegisterWorkerForm
+    template_name = "registration/register.html"
+    success_url = reverse_lazy("task_manager:index")
