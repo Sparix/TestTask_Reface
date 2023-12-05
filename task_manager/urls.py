dc4c4523462215e6dc205b04f4fabc2ebc5c464c
Olexandr Hontarenko <sanya221138@gmail.com>
2023-12-05 15:04:13 +0200

    add a view to create a new task and add the path to this view in urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -6,6 +6,8 @@ from task_manager.views import (
     RegisterNewWorkerView,
     TableUserListView,
     TableUserDetailView,
+    CreateNewTaskView,
+
 )
 
 urlpatterns = [
@@ -14,6 +16,7 @@ urlpatterns = [
     path("logout/", logout_user, name="logout"),
     path("work/tasks/", TableUserListView.as_view(), name="table-user"),
     path("task/<int:pk>/", TableUserDetailView.as_view(), name="detail-task"),
+    path("create/task/", CreateNewTaskView.as_view(), name="create-task"),
 
 ]
 app_name = "task_manager"
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,13 +1,15 @@
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 
-from task_manager.forms import RegisterWorkerForm
+from task_manager.forms import RegisterWorkerForm, CreateNewTaskForm
 from task_manager.models import Worker, Task
 
 
+@login_required
 def index(request):
     return render(request, "task_manager/index.html")
 
@@ -25,15 +27,26 @@ class RegisterNewWorkerView(generic.CreateView):
     success_url = reverse_lazy("task_manager:index")
 
 
-class TableUserListView(generic.ListView):
+class TableUserListView(LoginRequiredMixin, generic.ListView):
     model = Task
     context_object_name = "tables"
 
     def get_queryset(self):
-        queryset = Task.objects.prefetch_related("assignees").select_related("task_type").filter(
-            assignees=self.request.user)
+        queryset = (
+            Task.objects.prefetch_related(
+                "assignees").select_related(
+                "task_type").filter(
+                assignees=self.request.user
+            )
+        )
         return queryset
 
 
-class TableUserDetailView(generic.DetailView):
+class TableUserDetailView(LoginRequiredMixin, generic.DetailView):
     model = Task
+
+
+class CreateNewTaskView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    form_class = CreateNewTaskForm
+    success_url = reverse_lazy("task_manager:table-user")
