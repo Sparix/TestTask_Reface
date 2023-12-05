b7322444932f2ca1b588738018faca8539ce897d
Olexandr Hontarenko <sanya221138@gmail.com>
2023-12-05 15:01:01 +0200

    add a form for creating a task

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,9 +1,51 @@
-from django.contrib.auth.forms import UserCreationForm
+from datetime import datetime
 
-from task_manager.models import Worker
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+from task_manager.models import Worker, Task, CHOICES_PRIORITY
 
 
 class RegisterWorkerForm(UserCreationForm):
     class Meta:
         model = Worker
         fields = ("username", "email", "password1", "password2",)
+
+
+class CreateNewTaskForm(forms.ModelForm):
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple(attrs={
+            "id": "cb1",
+            "class": "assignees-checkbox",
+        }),
+    )
+
+    class Meta:
+        model = Task
+        fields = "__all__"
+        widgets = {
+            "deadline": forms.DateTimeInput(
+                attrs={
+                    "type": "datetime-local",
+                    "min": datetime.now(),
+                    "class": "deadline-input"
+                }
+            ),
+            "description": forms.Textarea(
+                attrs={
+                    "class": "text-area-description",
+                }
+            ),
+            "priority": forms.Select(
+                attrs={
+                    "class": "choice-select"
+                }
+            ),
+            "task_type": forms.Select(
+                attrs={
+                    "class": "choice-select"
+                }
+            ),
+        }
