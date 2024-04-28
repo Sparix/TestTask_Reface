78b0463cd422d4d2b2c7dd6b977fc6ca22d532a8
Olexandr Hontarenko <sanya221138@gmail.com>
2024-04-28 13:44:42 +0300

    fix bug with invisible text in form, and dark mode

--- static/js/dark-mode-handler.js
+++ static/js/dark-mode-handler.js
@@ -2,56 +2,36 @@ const themeSwitch = document.getElementById("theme-switch");
 const themeIndicator = document.getElementById("theme-indicator");
 const page = document.body;
 
-const themeStates = ["light", "dark"]
-const indicators = ["fa-moon", "fa-sun"]
-const pageClass = ["bg-gray-100", "dark-page"]
-
-let currentTheme = localStorage.getItem("theme");
+const themeStates = ["light", "dark"];
+const indicators = ["fa-moon", "fa-sun"];
+const pageClass = ["bg-gray-100", "dark-page"];
 
 function setTheme(theme) {
-    localStorage.setItem("theme", themeStates[theme])
+    localStorage.setItem("theme", themeStates[theme]);
 }
 
 function setIndicator(theme) {
-    themeIndicator.classList.remove(indicators[1])
-    themeIndicator.classList.remove(indicators[1])
-    themeIndicator.classList.add(indicators[theme])
+    themeIndicator.classList.remove(indicators[1]);
+    themeIndicator.classList.remove(indicators[0]);
+    themeIndicator.classList.add(indicators[theme]);
 }
 
 function setPage(theme) {
-    page.classList.remove(pageClass[1])
-    page.classList.remove(pageClass[1])
-    page.classList.add(pageClass[theme])
+    page.classList.remove(pageClass[0]);
+    page.classList.remove(pageClass[1]);
+    page.classList.add(pageClass[theme]);
 }
 
+// За замовчуванням встановлюємо тему "dark"
+setTheme(1);
+setIndicator(1);
+setPage(1);
+themeSwitch.checked = true; // Позначаємо перемикач як ввімкнений
 
-if (currentTheme === null) {
-    localStorage.setItem("theme", themeStates[1])
-    setIndicator(1)
-    setPage(1)
-    themeSwitch.checked = false;
-}
-if (currentTheme === themeStates[1]) {
-    setIndicator(1)
-    setPage(1)
-    themeSwitch.checked = false;
-
-}
-if (currentTheme === themeStates[1]) {
-    setIndicator(1)
-    setPage(1)
-    themeSwitch.checked = true;
-}
-
-
+// Обробник події для зміни теми при взаємодії з перемикачем
 themeSwitch.addEventListener('change', function () {
-    if (this.checked) {
-        setTheme(1)
-        setIndicator(1)
-        setPage(1)
-    } else {
-        setTheme(1)
-        setIndicator(1)
-        setPage(1)
-    }
+    const themeIndex = this.checked ? 1 : 0; // Визначаємо індекс теми на основі положення перемикача
+    setTheme(themeIndex);
+    setIndicator(themeIndex);
+    setPage(themeIndex);
 });
